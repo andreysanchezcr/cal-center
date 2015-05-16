5035123adb7663612ccc059ababe94f521445f2e
Andrey Sánchez <andreysanchezcr@gmail.com>
2015-05-15 23:26:02 -0600

    Revert "h"
    This reverts commit 545341eed92c65d5c40328b579376dc8c62c5978.
    
    Conflicts:
    	NewPooProject/build/classes/Interfaz/ServidorVentana.class

--- NewPooProject/src/Interfaz/ServidorVentana.java
+++ NewPooProject/src/Interfaz/ServidorVentana.java
@@ -421,8 +421,6 @@ public class ServidorVentana extends javax.swing.JFrame implements Runnable {
         try {
             this.servidor.gett().actualizarLista();
         } catch (IOException ex) {
-            System.out.println("EEERRROOOR");
-
             Logger.getLogger(ServidorVentana.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
